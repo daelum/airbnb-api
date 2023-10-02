5004639608c09dc7c5126502ca5d37197f30d96d
Daelum Marx <33014828+daelum@users.noreply.github.com>
2023-10-02 09:50:38 +0700

    Created model and schema for Users

---
+++ models/users.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const ObjectId = mongoose.Schema.Types.ObjectId
+
+const Users = mongoose.model('users', {
+    avatar: String,
+    email: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
