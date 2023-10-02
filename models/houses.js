9e20ff62062c6a27fdb13d48fcab5c886e84bdbd
Daelum Marx <33014828+daelum@users.noreply.github.com>
2023-10-02 09:50:50 +0700

    Created Model & Schema for Houses

---
+++ models/houses.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+const ObjectId = mongoose.Schema.Types.ObjectId
+
+const Houses = mongoose.model('houses', {
+    description: {
+        type: String,
+        required: true
+    },
+    host: {
+        type: ObjectId,
+        ref: 'users',
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },  
+    photos: [
+        {
+        type: String,
+        }
+    ],
+    price: {
+        type: Number,
+        required: true
+    },
+    rooms:  {
+        type: Number,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    }
+})
