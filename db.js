923ae1c7b2968c0c1915c7994488d7443184643b
Daelum Marx <33014828+daelum@users.noreply.github.com>
2023-10-02 09:50:12 +0700

    Updated DB url

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
 module.exports = {
-    DB_URL: 'mongodb+srv://daelum:fVzGwRXQrzHeF1Yu@cluster0.tnfj5io.mongodb.net/',
+    DB_URL: 'mongodb+srv://daelum:fVzGwRXQrzHeF1Yu@cluster0.tnfj5io.mongodb.net/airbnb',
     SESSION_SECRET: 'YOUR_SECRET'
 }
