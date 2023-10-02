706e7a9e0f27a6b8ff40b11e233de6f408909e18
Daelum Marx <33014828+daelum@users.noreply.github.com>
2023-10-02 09:51:07 +0700

    Created Model & Schema for Reviews

---
+++ models/reviews.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+const ObjectId = mongoose.Schema.Types.ObjectId
+
+const Reviews = mongoose.model('reviews', {
+    author: {
+        type: ObjectId,
+        ref: 'users',
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    house: {
+        type: ObjectId,
+        ref: 'houses',
+        required: true,
+    },
+    rating: {
+        type: Number,
+    }
+})
