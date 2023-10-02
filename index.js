34fe8ab852d35dc6c164f3ed99fc8968a0984cee
Daelum Marx <33014828+daelum@users.noreply.github.com>
2023-10-02 15:15:06 +0700

    fixed typo

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const { DB_URL } = require('./db')
 
 // import models
 const Users = require('./models/users')
-const Houses = require('./modles/houses')
+const Houses = require('./models/houses')
 const Reviews = require('./models/reviews')
 const Bookings = require('./models/bookings')
 
@@ -114,7 +114,9 @@ app.post('/login', async (req, res) => {
 })
 
 app.post('/signup', async (req, res) => {
+  const users = await Users.create(req.body)
   console.log(req.body);
+  res.send(users)
   console.log('hello im signup');
 })
 
